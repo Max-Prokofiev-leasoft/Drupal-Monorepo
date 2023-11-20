305ae70e8e7006715dad4b22a3848e1b038d4982
ProkofievLeasoft <max.prokofiev@leasoft.org>
2023-11-20 20:03:07 +0200

    Add core and bank functionality for ems

---
+++ ems/core/src/Redefiner/AbstractPaymentRedefiner.php
@@ -0,0 +1,17 @@
+<?php
+namespace Drupal\commerce_ginger\Redefiner;
+
+use Drupal\commerce_ginger\PluginForm\AbstractPayment;
+
+/**
+ * Class AbstractPaymentRedefiner.
+ *
+ * This class for redefining AbstractPayment
+ *
+ * @package Drupal\commerce_ginger\Redefiner
+ */
+
+class AbstractPaymentRedefiner extends AbstractPayment
+{
+  // Here you can redefine all AbstractPayment functionality
+}
